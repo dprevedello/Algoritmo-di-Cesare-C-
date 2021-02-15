ac59bfdf78b02f4241b682524cd975697020fc2a
Daniele Prevedello <prevedello@altervista.org>
2021-02-15 23:07:31 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char frase[500];
+
+    printf("Inserire una frase da cifrare: ");
+    scanf("%s", frase);
+
+    for(int i=0; frase[i]!='\0'; i++){
+        if( frase[i]>='a' && frase[i]<='z' )
+            frase[i] = (frase[i] - 'a' + 3) % 26 + 'a';
+        if( frase[i]>='A' && frase[i]<='Z' )
+            frase[i] = (frase[i] - 'A' + 3) % 26 + 'A';
+    }
+    
+    printf("La frase cifrata è: %s\n", frase);
+
+    return 0;
+}
