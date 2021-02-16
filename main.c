808f01f1f895013ff8813e5d1594ecc8101518e4
Daniele Prevedello <dprevedello86@gmail.com>
2021-02-16 00:18:34 +0000

    Removed scanf

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
     char frase[500];
 
     printf("Inserire una frase da cifrare: ");
-    scanf("%s", frase);
+    fgets(frase, sizeof(frase), stdin);
+    frase[strlen(frase)-1] = '\0';
 
     for(int i=0; frase[i]!='\0'; i++){
         if( frase[i]>='a' && frase[i]<='z' )
